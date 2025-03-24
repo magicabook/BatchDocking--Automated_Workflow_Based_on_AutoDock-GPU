7e7ecb05687d0b7d068f8949aa3ac4c50351498b
magicabook <88999566+magicabook@users.noreply.github.com>
2025-03-24 19:44:10 +0800

    Changelog:
    更新日志：
    1）实装自动识别并将dlg结果文件中的最优构象与蛋白质文件一起输出为复合物的功能，让您再也不必在文本编辑器和充满BUG的AutoDockTools中来回切换与选择。
    2）将蛋白质与小分子名称使用单独颜色表示，让您的终端更加简洁且一目了然。
    3）修复了一些已知BUG。
    4）看了一些猫咪视频，给校园里的猫猫拍了照。
    5）移除了Herobrine。
    
    Changelog:
    1) Implemented automatic recognition and output of the optimal conformation from dlg result files together with the protein file as a complex, eliminating the need to switch between text editors and the bug-ridden AutoDockTools.​
    2) Assigned distinct colors to protein and small molecule names, making your terminal more concise and intuitive.​
    3) Fixed some known bugs.​
    4) Watched some cat videos and took photos of cats on campus.​
    5) Removed Herobrine.

--- dock.py
+++ dock.py
@@ -6,8 +6,8 @@ import datetime
 import subprocess
 
 # Define and Set Your Work_Path and Basic Function
-csv_name_hat = 'work1'
-nrun = '100' #Docking run number
+csv_name_hat = 'test'
+nrun = '1' #Docking run number
 py2 = '/usr/bin/python2.7'
 AD_GPU = '/root/AutoDock-GPU-1.6/bin/autodock_gpu_128wi' # AutoDock-GPU path
 Grid = '/root/mgltools_x86_64Linux2_1.5.7/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py'
@@ -17,7 +17,8 @@ ligand_smi_single = '../ligand_smi_single' # single.smi path
 ligand_pdb = '../ligand_pdb' # .pdb path
 ligand_pdbqt_path = '../ligand_pdbqt' # .pdbqt path
 result_dlg = '../result_dlg/' # AutoDock-GPU run docking output for path
-result_csv = "../result_csv/" # sult csv path
+result_csv = '../result_csv/' # sult csv path
+result_complex = '../result_complex/'
 time_raw = datetime.datetime.now() # get system time to make file sign
 time_str = time_raw.strftime('%Y-%m-%d %H-%M-%S') # change to str
 ligand_number = 0 # give ligand a number , to save as csv
@@ -25,7 +26,7 @@ ligand_number = 0 # give ligand a number , to save as csv
 # Checking protein.maps.fld
 for protein_name in os.listdir(protein_list_path): # make protein list
     if os.system(f'find {protein_list_path}{protein_name}/ -type f -name "*.maps.fld" | grep -q .;') != 0:
-        print(f'\033[91mERROR: Not Find .maps.fld > {protein_name}\033[0m')
+        print(f'\033[91mERROR: Not Find .maps.fld > \033[96m{protein_name}\033[0m')
 
 # Make result_dlg/protein
 for protein_name in os.listdir(protein_list_path): # make protein list
@@ -33,10 +34,21 @@ for protein_name in os.listdir(protein_list_path): # make protein list
     checking_protein_cmd = f'ls {result_dlg}'
     checking_protein_out = subprocess.check_output(checking_protein_cmd, shell=True, stderr=subprocess.STDOUT, text=True)
     if protein_name in checking_protein_out:
-        print(f'\033[92m  have result catalog > {protein_name}\033[0m')  # print result catalog
+        print(f'\033[92m  have result_dlg catalog > \033[96m{protein_name}\033[0m')  # print result catalog
     else:
         os.system(f'mkdir {result_dlg}{protein_name}') #make result file
-        print(f'\033[92m  make result catalog > {protein_name}\033[0m')  # print result catalog
+        print(f'\033[92m  make result_dlg catalog > \033[96m{protein_name}\033[0m')  # print result catalog
+
+# Make result_complex/protein
+for protein_name in os.listdir(protein_list_path): # make protein list
+    # checking do .result_dlg have protein ?
+    checking_complex_cmd = f'ls {result_complex}'
+    checking_protein_out = subprocess.check_output(checking_complex_cmd, shell=True, stderr=subprocess.STDOUT, text=True)
+    if protein_name in checking_protein_out:
+        print(f'\033[92m  have result_complex catalog > \033[96m{protein_name}\033[0m')  # print result catalog
+    else:
+        os.system(f'mkdir {result_complex}{protein_name}') #make result file
+        print(f'\033[92m  make result_complex catalog > \033[96m{protein_name}\033[0m')  # print result catalog
 
 # Set result csv file
 csv_name = f'[{csv_name_hat}] {time_str}.csv'
@@ -81,7 +93,7 @@ for protein_name in os.listdir(protein_list_path): # make protein list
                     end = pdbqt_path.rfind('.')
                     ligand_name = pdbqt_path[start + 1:end]
                     ligand_number += 1  # give ligand number
-                    print(f'Docking proceece number : {ligand_number} Factor : {protein_name} and {ligand_name}')
+                    print(f'Docking proceece number : {ligand_number} Factor : \033[96m{protein_name}\033[0m and \033[95m{ligand_name}\033[0m')
                     result_path = f'{result_dlg}{protein_name}/'
                     dock_cmd = [AD_GPU,
                                 '--ffile', protein_path,
@@ -89,14 +101,32 @@ for protein_name in os.listdir(protein_list_path): # make protein list
                                 '-nrun', nrun,
                                 '-resnam', result_path]
                     dock_out = subprocess.check_output(dock_cmd, shell=False, stderr=subprocess.STDOUT, text=True)
-                    pattern = r'([-+]?[0-9]*\.?[0-9]+)\s*kcal/mol'  # fund *kcal/mol
+                    result_energy = r'([-+]?[0-9]*\.?[0-9]+)\s*kcal/mol'  # fund *kcal/mol
                     # fund all *kcal/mol
-                    result_class = re.findall(pattern, dock_out)  # all docking energy result
+                    result_class = re.findall(result_energy, dock_out)  # all docking energy result
                     if result_class:
                         # abstracting(extracted) last
                         energy = result_class[-1]
                         energy_unit = f'{energy} kcal/mol'
-                        print(f'\033[92m  {ligand_name} Doking {protein_name} Successful! > Bast_Low_Energy: {energy}\033[0m')  # print docking result
+                        print(f'  \033[95m{ligand_name} \033[92mDoking \033[96m{protein_name} \033[92mSuccessful! > Bast_Low_Energy: {energy}\033[0m')  # print docking result
+                        # abstracting dlg and out put complex pdbqt
+                        dlg_path = f'{result_dlg}{protein_name}/{ligand_name}.dlg' # find dlg file
+                        ER_bast = fr"Estimated Free Energy of Binding\s*=\s*{energy}\s*kcal/mol(.*?)(?=DOCKED: ENDMDL)" # Expression for find bast low energy data
+                        with open(dlg_path, "r", encoding="utf-8") as f_dlg:
+                            dlg_data = f_dlg.read()
+                        match = re.search(ER_bast, dlg_data, re.DOTALL)
+                        extracted = match.group(1)
+                        ligand_pdbqt_data = extracted.replace("DOCKED: ", "") # this complex.pdbqt in ligand part
+                        # find and read protein.pdbqt
+                        for f in os.listdir(f'{protein_list_path}{protein_name}/'):
+                            if f.endswith('.pdbqt'):
+                                protein_pdbqt_name = f
+                        with open(f'{protein_list_path}{protein_name}/{protein_pdbqt_name}', "r", encoding="utf-8") as protein_pdbqt:
+                            protein_pdbqt_data = protein_pdbqt.read()
+                        complex_data = protein_pdbqt_data + "\n" + ligand_pdbqt_data
+                        with open(f'{result_complex}{protein_name}/{protein_name}-{ligand_name}-complex.pdbqt', "w", encoding="utf-8") as fout:
+                            fout.write(complex_data)
+                        print(f'\033[92m  complex file make Successful!\033[0m')
                         # writer energy to csv
                         # read csv_data_2d git positioning
                         x_coordinates = first_line_list.index(protein_name)
@@ -104,13 +134,13 @@ for protein_name in os.listdir(protein_list_path): # make protein list
                         csv_data_2d[y_coordinates][x_coordinates] = energy # writer energy to csv
                     else:
                         energy = "Not found"
-                        print(f'\033[91mERROR: docking Failure! Error Molecular > {ligand_name}\n vvvvv Pleace Read more Information! vvvvv \n<<<=====[Process out put:]=====>>>\n{dock_out}\033[0m')  # print red error
-        else:
-            error_pdbqt_path = os.path.join(ligand_pdbqt_path, ligand_name)  # fund error file list
-            error_pqbqt_name = error_pdbqt_path.rsplit('/', 1)[-1]  # set error file name
-            print('\033[93m' + '  WARNING: Pleace Clear Error Type File > ligand_pdbqt >' + error_pqbqt_name + '\033[0m')  # print red error
+                        print(f'\033[91mERROR: docking Failure! Error Molecular > \033[95m{ligand_name}\n \033[91mvvvvv Pleace Read more Information! vvvvv \n<<<=====[Process out put:]=====>>>\n{dock_out}\n^^^^^ Pleace Read more Information! ^^^^^ \033[0m')  # print red error
+    else:
+        error_pdbqt_path = os.path.join(ligand_pdbqt_path, ligand_name)  # fund error file list
+        error_pqbqt_name = error_pdbqt_path.rsplit('/', 1)[-1]  # set error file name
+        print('\033[93m' + '  WARNING: Pleace Clear Error Type File > ligand_pdbqt >' + error_pqbqt_name + '\033[0m')  # print red error
 # Energy result (csv_data_2d) writer to csv file
 with open(csv_path, 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
     writer.writerows(csv_data_2d)
-print(f'\033[92m  Energy result (csv_data_2d) writer to csv Successful! > {csv_name}\033[0m')
+print(f'\033[92m  Energy result writer to csv Successful! > {csv_name}\033[0m')
